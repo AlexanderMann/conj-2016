5b0233eb47c5531b39fcdc05f721c43926dc5044
AlexanderMann <alex@circleci.com>
2016-10-17 22:52:10 -0400

    init

---
+++ src/conj_2016/core.clj
@@ -0,0 +1,6 @@
+(ns conj-2016.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/conj_2016/core_test.clj
@@ -0,0 +1,7 @@
+(ns conj-2016.core-test
+  (:require [clojure.test :refer :all]
+            [conj-2016.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
