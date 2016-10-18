646a94c89fc10d43075f269de4f92e0c6f9df46e
AlexanderMann <alex@circleci.com>
2016-10-17 23:39:25 -0400

    adding parser for embeddings

--- src/conj_2016/core.clj
+++ src/conj_2016/core.clj
@@ -1,6 +1,25 @@
-(ns conj-2016.core)
+(ns conj-2016.core
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string])
+  (:import [java.util.zip GZIPInputStream]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn gunzip
+  [path]
+  (with-open [in (GZIPInputStream.
+                  (clojure.java.io/input-stream
+                   path))]
+    (slurp in)))
+
+(defn parse-embeddings
+  [path]
+  (let [unzipped (gunzip path)
+        lines (string/split-lines unzipped)
+        line-parser (fn [v l]
+                      (let [[c & nums] (string/split l #" ")]
+                        (assert (not (contains? v c))
+                                (format "%s already in parsed embeddings. Duplicates are not allowed" c))
+                        (assoc v
+                          c (map read-string nums))))]
+    (reduce line-parser {} lines)))
+
+(def inp (parse-embeddings (io/resource "english-embeddings.turian.txt.gz")))
