c067240fa7fdb8fe728095bf20e73c022384d7a8
AlexanderMann <alex@circleci.com>
2017-03-02 08:58:20 -0600

    success on the previous blackout, trying next least likely culprits

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :resource-paths ["resources"]
-  :dependencies [;[cheshire "5.7.0"]
+  :dependencies [[cheshire "5.7.0"]
                  ;[clatrix "0.5.0"]
-                 ;[com.taoensso/timbre "4.8.0"]
-                 ;[com.taoensso/tufte "1.1.1"]
+                 [com.taoensso/timbre "4.8.0"]
+                 [com.taoensso/tufte "1.1.1"]
                  ;[dali "0.7.3"]
                  ;[incanter "1.9.1"]
-                 ;[net.mikera/core.matrix "0.57.0"]
+                 [net.mikera/core.matrix "0.57.0"]
                  [org.clojure/clojure "1.9.0-alpha14"]
                  [org.clojure/core.memoize "0.5.9"]
                  [org.clojure/data.csv "0.1.3"]
